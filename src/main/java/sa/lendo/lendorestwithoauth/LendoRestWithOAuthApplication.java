030436648ff5d4d46820fa9cdc8ce16f12218a70
ahmedelzubair <ahmed.aau3@gmail.com>
2022-10-25 11:38:39 +0200

    init commit all required dependencies added

---
+++ src/main/java/sa/lendo/lendorestwithoauth/LendoRestWithOAuthApplication.java
@@ -0,0 +1,13 @@
+package sa.lendo.lendorestwithoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendoRestWithOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendoRestWithOAuthApplication.class, args);
+    }
+
+}
